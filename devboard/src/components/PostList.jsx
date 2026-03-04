bfb0cb3dc6e11e2911d19817bd554f3ff608dcc0
watcharapong <67160198@go.buu.ac.th>
2026-03-04 16:30:56 +0700

    PostList Component

---
+++ devboard/src/components/PostList.jsx
@@ -0,0 +1,24 @@
+import PostCard from "./PostCard";
+import PostCount from "./PostCount";
+
+function PostList({ posts }) {
+  return (
+    <div>
+      <h2
+        style={{
+          color: "#feffff",
+          borderBottom: "2px solid #1e40af",
+          paddingBottom: "0.5rem",
+        }}
+      >
+        โพสต์ล่าสุด
+      </h2>
+      <PostCount count={posts.length} />
+      {posts.map((post) => (
+        <PostCard key={post.id} title={post.title} body={post.body} />
+      ))}
+    </div>
+  );
+}
+
+export default PostList;
